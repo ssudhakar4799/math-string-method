9c54d99591888f74fdfb8aa39aff60e2bca2b97d
ssudhakar4799 <104546063+ssudhakar4799@users.noreply.github.com>
2022-07-31 00:27:26 +0530

    Add files via upload

---
+++ inbuild-functions/inbuild.js
@@ -0,0 +1,106 @@
+//abs
+x=Math.abs(-85);
+console.log('abs=',x);
+
+//ceil
+x=Math.ceil(85.67);
+console.log('ceil=',x);
+
+//floor
+x=Math.floor(86.67);
+console.log(x);
+
+//max
+x=Math.max(45,76.64,98);
+console.log('max=',x);
+
+//min
+x=Math.min(867,96,34,42,12);
+console.log('min=',x);
+
+//pow
+x=Math.pow(6,3);
+console.log('pow=',x);
+
+//random
+x=Math.random(3,4);
+console.log('random=',x);
+
+//round
+x=Math.round(3.5);
+console.log('round=',x);
+
+//sqrt
+x=Math.sqrt(49);
+console.log('sqrt=',x);
+
+// string
+//chartAt
+var x='sudhakar'
+var y=x.charAt(5);
+console.log(y);
+
+//touppercase
+var x="sudhakar"
+var y=x.toUpperCase();
+console.log(y);
+
+//concat
+var x='sudha'
+var y='kar'
+var z=x.concat(y);
+console.log(z);
+
+//include
+var x=['cat','lion','dog'];
+console.log(x.includes('dog'));
+
+//< String >
+//charAt
+var s="sudhakar";
+var y=s.charAt(2);
+console.log('charAt=',y);
+
+//uppercase
+var s="sudhakar";
+var y=s.toUpperCase();
+console.log('uppercase=',y);
+
+//concat
+var a="sudha";
+var b="kar";
+var c=a.concat(b);
+console.log('concat=',c);
+
+//includes
+var pet=['dog','cat','lion'];
+console.log(pet.includes('parrot'));
+
+//indexOf
+var friends=['Gnanasekar','Arun','Sudhakar','Francis','Venkat','Deepak'];
+console.log(friends.indexOf('Arun'));
+
+//lastIndexOf
+var frd=['Gnanasekar','Arun','Sudhakar','Francis','sudhakar','Arun','Venkat','Deepak'];
+console.log(frd.lastIndexOf('sudhakar'));
+
+//Replace
+var p='my name is sudhakar gnanasekar';
+console.log(p.replace('my name is sudhakar','en peyar cr7'));
+
+//raplaceAll
+var Q='my name is sudhakar.i am good cricketer';
+console.log(Q.replaceAll('sudhakar','gnanasekarr'));
+
+//slice
+var m='msdhoniisagoodcaptain';
+console.log(m.slice(2));
+
+//starts with
+var r='survival of the fitest';
+console.log(r.startsWith('survival'));
+
+//sustring
+var t='universal solvent for water';
+console.log(t.substring('6'));
+
